340cc6380c10fc9b7091f7694c14339b7078d537
Juanes <ejdelacruz@uninorte.edu.co>
2017-08-29 07:31:40 -0500

    iconos

--- src/Source/home.java
+++ src/Source/home.java
@@ -28,10 +28,18 @@ public class home extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        btn_usuarios = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        btn_registro = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        btn_usuarios = new javax.swing.JLabel();
+        jPanel4 = new javax.swing.JPanel();
+        btn_registro1 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jPanel5 = new javax.swing.JPanel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -39,39 +47,83 @@ public class home extends javax.swing.JFrame {
         jPanel1.setBackground(new java.awt.Color(97, 212, 195));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Transacciones");
+        jPanel2.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 120, 160, -1));
+
+        jLabel5.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel5.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/transaccionIcon.png"))); // NOI18N
+        jLabel5.setOpaque(true);
+        jPanel2.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 160, 110));
+
+        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 60, 160, 150));
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Usuarios");
+        jPanel3.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 120, 160, -1));
+
         btn_usuarios.setBackground(new java.awt.Color(255, 255, 255));
         btn_usuarios.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
         btn_usuarios.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        btn_usuarios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/userIcon.png"))); // NOI18N
-        btn_usuarios.setText("Usuarios");
+        btn_usuarios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/usuariosIcon.png"))); // NOI18N
         btn_usuarios.setOpaque(true);
         btn_usuarios.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 btn_usuariosMouseClicked(evt);
             }
         });
-        jPanel1.add(btn_usuarios, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 90, 160, 120));
+        jPanel3.add(btn_usuarios, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 160, 120));
 
-        jLabel3.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel3.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("Prestamos");
-        jLabel3.setOpaque(true);
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 300, 160, 120));
+        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 60, 160, 150));
 
-        btn_registro.setBackground(new java.awt.Color(255, 255, 255));
-        btn_registro.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
-        btn_registro.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        btn_registro.setText("Registro");
-        btn_registro.setOpaque(true);
-        jPanel1.add(btn_registro, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 300, 160, 120));
+        jPanel4.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel5.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel5.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
-        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel5.setText("Transacciones");
-        jLabel5.setOpaque(true);
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 90, 160, 120));
+        btn_registro1.setBackground(new java.awt.Color(255, 255, 255));
+        btn_registro1.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
+        btn_registro1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        btn_registro1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/registrosIcon.png"))); // NOI18N
+        btn_registro1.setOpaque(true);
+        btn_registro1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_registro1MouseClicked(evt);
+            }
+        });
+        jPanel4.add(btn_registro1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 160, 120));
+
+        jLabel4.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setText("Registro");
+        jPanel4.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 120, 160, -1));
+
+        jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 300, 160, 150));
+
+        jPanel5.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel5.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel6.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
+        jLabel6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel6.setText("Prestamos");
+        jPanel5.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 120, 160, -1));
+
+        jLabel7.setBackground(new java.awt.Color(255, 255, 255));
+        jLabel7.setFont(new java.awt.Font("Tsukushi A Round Gothic", 1, 18)); // NOI18N
+        jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/prestamosIcon.png"))); // NOI18N
+        jLabel7.setOpaque(true);
+        jPanel5.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 160, 110));
+
+        jPanel1.add(jPanel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 300, 160, 150));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 820, 530));
 
@@ -85,6 +137,14 @@ public class home extends javax.swing.JFrame {
         users.setLocationRelativeTo(null);
         this.dispose();
     }//GEN-LAST:event_btn_usuariosMouseClicked
+
+    private void btn_registro1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_registro1MouseClicked
+        // TODO add your handling code here:
+        Registro reg = new Registro();
+        reg.setVisible(true);
+        reg.setLocationRelativeTo(null);
+        this.dispose();
+    }//GEN-LAST:event_btn_registro1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -122,10 +182,18 @@ public class home extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel btn_registro;
+    private javax.swing.JLabel btn_registro1;
     private javax.swing.JLabel btn_usuarios;
-    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
     // End of variables declaration//GEN-END:variables
 }
